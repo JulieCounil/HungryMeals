ffeb61af86fe9d7a498ba4f00bfa2a10cd447a23
JulieCounil <45055948+JulieCounil@users.noreply.github.com>
2018-11-15 04:32:50 +0100

    Update User.java

--- app/src/main/java/com/android/hungrymeals/Classes/User.java
+++ app/src/main/java/com/android/hungrymeals/Classes/User.java
@@ -4,11 +4,25 @@ import android.media.Image;
 
 public class User {
     private String mUserName;
+    public String email;
+    public String pwd;
     private Image mProfilPicture;
+
+
     public User(String name){
         mUserName = name;
        //mProfilPicture = profilPicture;
     }
+
+
+    public User(String mUserName, String email, String pwd) {
+        this.mUserName = mUserName;
+        this.email = email;
+        this.pwd =pwd;
+    }
+
+
+
 
     public String getUserName(){return mUserName;}
    // public Image getProfilePicture(){return mProfilPicture;}
