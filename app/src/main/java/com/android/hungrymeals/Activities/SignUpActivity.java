f0957f69277c7235b886c34e432dfb2c84213a37
JulieCounil <45055948+JulieCounil@users.noreply.github.com>
2018-11-19 10:05:17 +0100

    Update SignUpActivity.java

--- app/src/main/java/com/android/hungrymeals/Activities/SignUpActivity.java
+++ app/src/main/java/com/android/hungrymeals/Activities/SignUpActivity.java
@@ -8,7 +8,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.android.hungrymeals.Classes.User;
-import com.google.firebase.hungrymeals;
+//import com.google.firebase.hungrymeals;
 
 public class SignUpActivity  extends AppCompatActivity {
 
@@ -19,8 +19,8 @@ public class SignUpActivity  extends AppCompatActivity {
 
         setOnClickListener(R.id.Cancel, LoginActivity.class);
 
-        private hungrymeals mDataBase;
-        mDatabase= FirebaseDatabase.getInstance().getReference();
+       // private hungrymeals mDataBase;
+       // mDatabase= FirebaseDatabase.getInstance().getReference();
     }
 
     public void register(View view){
@@ -37,27 +37,27 @@ public class SignUpActivity  extends AppCompatActivity {
         EditText edit4= (EditText) findViewById(R.id.pwdTwo);
         String pwdTwo = edit1.getText().toString();
 
-        User user =mDataBase.child("users").child(name).getValue();
+      //  User user =mDataBase.child("users").child(name).getValue();
 
         if(pwdTwo != pwdOne){
             Toast.makeText(this, "Passwords aren't matching! Try again",Toast.LENGTH_LONG).show();
         }
 
 
-        else if(user !=null) {
-            if(user.getEmail()==email){
-                Toast.makeText(this, "Account with same userName and Email already exist!",Toast.LENGTH_LONG).show();
-            }
-            Toast.makeText(this, "Username already taken!",Toast.LENGTH_LONG).show();
-        } //if already in database
+    //    else if(user !=null) {
+     //       if(user.getEmail()==email){
+     //           Toast.makeText(this, "Account with same userName and Email already exist!",Toast.LENGTH_LONG).show();
+     //       }
+     //       Toast.makeText(this, "Username already taken!",Toast.LENGTH_LONG).show();
+    //    } //if already in database
 
-        else{
+  //    else{
             //add the user to database
-            writeNewUser(name, email, pwdOne);
+    //        writeNewUser(name, email, pwdOne);
 
 
             //go back to login page
-            Intent intent = new Intent(this, LoginActivity.class);
+        Intent intent = new Intent(this, LoginActivity.class);
             startActivity(intent);
 
         }
@@ -80,36 +80,36 @@ public class SignUpActivity  extends AppCompatActivity {
 
 
 
-    private void writeNewUser(String mUserName, String email, String pwd){
-        User user = new User(mUserName, email,pwd);
-        mDatabase.child("users").child(mUserName).setValue(user);
+ //   private void writeNewUser(String mUserName, String email, String pwd){
+ //       User user = new User(mUserName, email,pwd);
+ //       mDatabase.child("users").child(mUserName).setValue(user);
 
-    }
+ //   }
     
     
 
-    private void deleteUser(String mUserName, User user){
-        mDataBase.child("users").child(mUserName).removeValue(user);
-    }
+//    private void deleteUser(String mUserName, User user){
+//        mDataBase.child("users").child(mUserName).removeValue(user);
+//    }
 
 
-    private void login (User user) {
-        User usert =mDataBase.child("users").child(user.getUserName()).getValue();
+//    private void login (User user) {
+//        User usert =mDataBase.child("users").child(user.getUserName()).getValue();
 
-        if(usert==null){
-            Toast.makeText(this, "User does not exist",Toast.LENGTH_LONG).show();
+ //       if(usert==null){
+ //           Toast.makeText(this, "User does not exist",Toast.LENGTH_LONG).show();
 
-        }
-        else if (usert.getPwd() != user.getPwd()){
-            Toast.makeText(this, "Wrong password!",Toast.LENGTH_LONG).show();
+//        }
+ //       else if (usert.getPwd() != user.getPwd()){
+//            Toast.makeText(this, "Wrong password!",Toast.LENGTH_LONG).show();
 
-        }
-        else{
-            Toast.makeText(this, "Login succeed!",Toast.LENGTH_LONG).show();
+//        }
+//        else{
+//            Toast.makeText(this, "Login succeed!",Toast.LENGTH_LONG).show();
             
-            //go to general menu
-            Intent intent = new Intent(this,MainActivity.class);
-            startActivity(intent);
+//            //go to general menu
+//            Intent intent = new Intent(this,MainActivity.class);
+ //           startActivity(intent);
 
         } }
 
